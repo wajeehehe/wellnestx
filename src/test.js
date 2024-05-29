4d5cda933901668caf9e64b6633fe774b4dc8ff7
laiba <laibanaveed458@gmail.com>
2024-05-30 01:42:18 +0500

    hi

--- src/test.js
+++ src/test.js
@@ -9,6 +9,18 @@ const AppointmentList = () => {
     const [filteredAppointments, setFilteredAppointments] = useState([]);
     const [unfilteredappointments, setUnfilteredAppointments] = useState([]);
     const [keyword, setKeyword] = useState();
+    const [timeSlots, setTimeSlots] = useState([
+        { time: 15, available: true },
+        { time: 16, available: true },
+        { time: 17, available: true },
+        { time: 18, available: true },
+        { time: 19, available: true },
+        { time: 20, available: true },
+        { time: 21, available: true },
+        { time: 22, available: true },
+        { time: 23, available: true },
+        { time: 24, available: true },
+    ]);
 
     const handleSearchChange = (event) => {
         setKeyword(event.target.value);
@@ -22,6 +34,7 @@ const AppointmentList = () => {
             const appointmentList = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
             setUnfilteredAppointments(appointmentList);
             console.log("appointments")
+
         };
         getAppointmentsData();
 
@@ -32,15 +45,27 @@ const AppointmentList = () => {
             const filteredAppointment = unfilteredappointments.filter((appointment) =>
                 appointment.DocID.toLowerCase().includes(keyword) || appointment.PatientID.toLowerCase().includes(keyword)
             );
+            // setFilteredAppointments(filteredAppointment);
+            const updatedTimeSlots = timeSlots.map((timeSlot) => {
+                const isBooked = filteredAppointment.some((appointment) => appointment.Time == timeSlot.time);
+
+                console.log(timeSlot.time);
+                return { ...timeSlot, available: !isBooked };
+            });
+
             setFilteredAppointments(filteredAppointment);
+            setTimeSlots(updatedTimeSlots);
         }
         else {
             setFilteredAppointments(unfilteredappointments);
+            setTimeSlots(timeSlots.map((slot) => ({ ...slot, available: true }))); // Reset availability on empty search
         }
-        console.log("Filtered")
-        //getFilteredAppointments();
+
+
 
     }, [keyword, unfilteredappointments]);
+
+
 
     return (
         <div>
@@ -54,7 +79,7 @@ const AppointmentList = () => {
                     <li key={appointment.id}>
                         {/* Display appointment details */}
                         {appointment.date} May | {appointment.Time} PM | Dr ID. {appointment.DocID} | Pt ID.  {appointment.PatientID} |
-
+                        {(timeSlots.time)?.available ? "Available" : "Unavailable"}
 
                     </li>
                 ))}
